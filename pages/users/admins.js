d4f3da9d550d710b8fb72e8fbfc1d9846159e0d9
Nicolas <pnwa@icloud.com>
2022-01-01 18:21:16 +0100

    Add admins and authors to users route

---
+++ pages/users/admins.js
@@ -0,0 +1,9 @@
+function Admins() {
+    return (
+        <div>
+            <h1>Tous les admins</h1>
+        </div>
+    )
+}
+
+export default Admins
---
+++ pages/users/authors.js
@@ -0,0 +1,9 @@
+function Authors() {
+    return (
+        <div>
+            <h1>Tous les auteurs</h1>
+        </div>
+    )
+}
+
+export default Authors
