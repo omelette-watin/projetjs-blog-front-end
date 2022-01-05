8862c0035c20ab654733b26aff093d6c6a5d33cf
Nicolas <pnwa@icloud.com>
2022-01-05 04:53:13 +0100

    Add 'still developping' divÃ

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Navbar from "./navbar/Navbar"
 import Footer from "./footer/Footer"
 import { useUser } from "../user"
+import styles from './Layout.module.css'
 
 function Layout({ children, title }) {
     const { user } = useUser()
@@ -11,6 +12,9 @@ function Layout({ children, title }) {
                 <title>{title ?  title + " - " : null} Unicorn's Blog </title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
+            <div className={styles.dev_wrapper}>
+                👷 <span>Ce site est encore en développement</span> 👷
+            </div>
             <Navbar user={user} active={title} />
             <main style={{minHeight: '60vh'}}>{children}</main>
             <Footer/>
